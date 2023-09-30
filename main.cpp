77904627b8f6ad06247791e6e25b45512666a519
f-squirrel <dmitry.b.danilov@gmail.com>
2023-09-30 03:30:43 +0300

    feat: init tidy config

--- main.cpp
+++ main.cpp
@@ -1,4 +1,29 @@
+#include <string>
+
+namespace Filesystem {
+
+class file {
+public:
+    int GetId() const { return id; }
+    auto &GetName() const { return FileName_; }
+
+private:
+    int id;
+    std::string FileName_;
+};
+
+struct Permissions {
+    bool read_;
+    bool write;
+    bool Execute;
+};
+
+} // namespace Filesystem
 
 int main() {
-return 0;
+
+    auto permissions =
+        Filesystem::Permissions{.read_ = true, .write = true, .Execute = false};
+    (void)permissions;
+    return 0;
 }
